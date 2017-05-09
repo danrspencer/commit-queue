878e2abd5f822d1525e2891ebd52694c026711db
Dan Spencer <dan.spencer@anaplan.com>
2017-05-09 08:12:00 +0100

    Change of plan, creating a push script

--- scripts/approve.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Ensure git is setup correctly locally
-./setup.sh -s
-
-CURRENT_BRANCH=`git branch | grep \* | awk '{print $2}'`
-git push source ${CURRENT_BRANCH}
--- scripts/reject.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Ensure git is setup correctly locally
-./setup.sh -s
-
-BRANCH=`git branch | grep \* | awk '{print $2}'`
-
-git fetch source &> /dev/null
-git cherry source/${BRANCH}
